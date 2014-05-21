4e06c7d1960cec94c2458e4565fcc3c4307f8806
jnozsc <jnozsc@gmail.com>
2014-05-20 22:40:46 -0400

    first commit

---
+++ A_+_B_Problem.cpp
@@ -0,0 +1,19 @@
+/*
+For given numbers a and b in function aplusb, return the sum of them.
+Note
+You don't need to parse the input and output. Just calculate and return.
+Example
+If a=1 and b=2 return 3
+*/
+class Solution {
+  public:
+    /*
+     * @param a: The first integer
+     * @param b: The second integer
+     * @return: The sum of a and b
+     */
+    int aplusb(int a, int b) {
+        // Just submit this code, then you will get accepted!
+        return a + b;
+    }
+};
