fe12c836baef6bf7010b2b5a7a79bec1ce05ea2d
jnozsc <jnozsc@gmail.com>
2014-08-02 13:49:12 -0400

    nothing

--- Maximum_Subarray_Difference.cpp
+++ Maximum_Subarray_Difference.cpp
@@ -12,57 +12,57 @@ using namespace std;
 class Solution {
   public:
     /**
-     * @param nums: A list of integers
+     * @param v: A list of integers
      * @return: An integer indicate the value of maximum difference between two
      *          Subarrays
      */
-    int maxDiffSubArrays(vector<int> nums) {
+    int maxDiffSubArrays(vector<int> v) {
         // write your code here
         int tmpMin, tmpMax;
-        int maxTo[nums.size()];
-        int maxFrom[nums.size()];
-        int minTo[nums.size()];
-        int minFrom[nums.size()];
-        maxTo[0] = nums[0];
-        minTo[0] = nums[0];
-        tmpMin = nums[0];
-        tmpMax = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
+        int maxTo[v.size()];
+        int maxFrom[v.size()];
+        int minTo[v.size()];
+        int minFrom[v.size()];
+        maxTo[0] = v[0];
+        minTo[0] = v[0];
+        tmpMin = v[0];
+        tmpMax = v[0];
+        for (int i = 1; i < v.size(); i++) {
             if (tmpMax < 0) {
-                tmpMax = nums[i];
+                tmpMax = v[i];
             } else {
-                tmpMax += nums[i];
+                tmpMax += v[i];
             }
             maxTo[i] = max(tmpMax, maxTo[i - 1]);
             if (tmpMin >= 0) {
-                tmpMin = nums[i];
+                tmpMin = v[i];
             } else {
-                tmpMin += nums[i];
+                tmpMin += v[i];
             }
             minTo[i] = min(tmpMin, minTo[i - 1]);
         }
-        tmpMin = nums[nums.size() - 1];
-        tmpMax = nums[nums.size() - 1];
-        minFrom[nums.size() - 1] = nums[nums.size() - 1];
-        maxFrom[nums.size() - 1] = nums[nums.size() - 1];
-        for (int i = nums.size() - 2; i >= 0; i--) {
+        tmpMin = v[v.size() - 1];
+        tmpMax = v[v.size() - 1];
+        minFrom[v.size() - 1] = v[v.size() - 1];
+        maxFrom[v.size() - 1] = v[v.size() - 1];
+        for (int i = v.size() - 2; i >= 0; i--) {
             if (tmpMax < 0) {
-                tmpMax = nums[i];
+                tmpMax = v[i];
             } else {
-                tmpMax += nums[i];
+                tmpMax += v[i];
             }
             maxFrom[i] = max(tmpMax, maxFrom[i + 1]);
             if (tmpMin >= 0) {
-                tmpMin = nums[i];
+                tmpMin = v[i];
             } else {
-                tmpMin += nums[i];
+                tmpMin += v[i];
             }
             minFrom[i] = min(tmpMin, minFrom[i + 1]);
         }
         int result = max(maxTo[0] - minFrom[1],  maxFrom[1] - minTo[0]);
-        for (int i = 1; i < nums.size() - 1; i++) {
+        for (int i = 1; i < v.size() - 1; i++) {
             result = max(result, max(maxTo[i] - minFrom[i + 1], maxFrom[i + 1] - minTo[i]));
         }
-        return result;
+        cout << result << endl;
     }
 };
